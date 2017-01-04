6762f3ad7e590f97a65fb5e223fc3db69499328e
alexsii <looxa233@gmail.com>
2017-01-04 23:58:09 +0200

    fix  admin panel

--- resources/views/admin/category/update.blade.php
+++ resources/views/admin/category/update.blade.php
@@ -7,10 +7,10 @@
                    'enctype' => 'multipart/form-data'
            ]) !!}
         {!! Form::label('email', 'Название события на русском', ['style' => 'margin: 10px 10px 0 30px;']) !!}
-        {!! Form::text('name_ru', null,['class' => 'form-control', 'style' => 'margin:10px 30px; width:60%;', 'placeholder' => 'Название категории на русском']) !!}
+        {!! Form::text('name_ru', null,['class' => 'form-control','required', 'style' => 'margin:10px 30px; width:60%;', 'placeholder' => 'Название категории на русском']) !!}
 
         {!! Form::label('email', 'Название события на казахском', ['style' => 'margin: 10px 10px 0 30px;']) !!}
-        {!! Form::text('name_kz', null,['class' => 'form-control', 'style' => 'margin:10px 30px; width:60%;', 'placeholder' => 'Название категории на казахском']) !!}
+        {!! Form::text('name_kz', null,['class' => 'form-control','required', 'style' => 'margin:10px 30px; width:60%;', 'placeholder' => 'Название категории на казахском']) !!}
         {!! Form::submit('Update', ['class' => 'form-control', 'style' => 'margin:10px 30px; width:60%;']) !!}
         {!! Form::close() !!}
     </main>
