3dfe1564c95f9a1f065992a1299f8cfb31941738
alexsii <looxa233@gmail.com>
2017-01-05 20:55:29 +0200

    fix  admin panel in logout

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,7 +13,6 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
-
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
